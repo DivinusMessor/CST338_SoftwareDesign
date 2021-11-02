a0258cb56c18b107651c02eba62df735215cd115
DivinusMessor <DivinusMessor@users.noreply.github.com>
2021-11-01 19:10:56 -0700

    added changes after going to ta office hours

--- stringManipulation.java
+++ stringManipulation.java
@@ -10,11 +10,11 @@ class stringManipulation
    public static void main( String[] args )
    {
       // Part 1
-      // Creating sc to take user input and requesting info
-      // Q: Should I add a new line for user input?
+      // Creating scanner to take user input and requesting info
       // Q: Should the output we put in the .txt file also include the questions that is asked to the user?
       // Q: Do we need to comment out the output examples in the .txt file?
       Scanner scan = new Scanner (System.in);
+      System.out.println ("Assignment Part 1");
       System.out.println ("Enter your first name and please capitalize "
       + "first letter ");
       String first = scan.next();
@@ -40,20 +40,19 @@ class stringManipulation
       int MAX_HOURS = 20;
       String pattern = "##.#";
       // Printing range of study hours
-      System.out.print ("Min hours to max study hours are: ");
+      System.out.println ("Assignment Part 2");
+      System.out.print ("Min to max study hours are: ");
       for (int i = MIN_HOURS; i <= MAX_HOURS; i++)
       {
          System.out.print (i + " ");
       }
       System.out.println("");
-      System.out.println ("How many hours did you should spend on this"
+      System.out.println ("How many hours did you spend on"
       + " class this week? Enter a number to 3 decimal places please ");
       float studyTime = scan.nextFloat();
       DecimalFormat numberFormat = new DecimalFormat(pattern);
       System.out.print ("Study hours rounded to 1 decimal place: ");
       System.out.println(numberFormat.format(studyTime));
-      // Q: Do we need to close?
       scan.close();
    }
 }
-
