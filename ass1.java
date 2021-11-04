a0a357059279cefb02ac8d84ae7fa00cd8f1a988
DivinusMessor <DivinusMessor@users.noreply.github.com>
2021-11-03 23:07:33 -0700

    added

---
+++ ass1.java
@@ -0,0 +1,25 @@
+class ass1
+{
+   String string1 = "";
+   String string2 = "";
+   String string3 = "";
+
+   public static int getBet()
+   {
+      System.out.println("GetBet");
+      return 0;
+   }
+   
+   public static ThreeString pull()
+   {
+      System.out.println("ThreeStringMethod");
+      return "Stringoutput";
+   }
+
+   public static void main ( String[] args ) 
+   {
+      System.out.println("hi");
+   }
+
+
+}
