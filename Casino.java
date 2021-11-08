821dbf58163fe3b0bb11647632f32608b80556b9
DivinusMessor <DivinusMessor@users.noreply.github.com>
2021-11-07 18:39:38 -0800

    almost done with getPayMultiplier()

--- Casino.java
+++ Casino.java
@@ -2,46 +2,138 @@
 // CST 338 - Software Design 
 // Assignment 2 - Casino
 
-class Casino
+import java.util.*;
+
+public class Casino
 {
+   public static void main(String[] args) 
+   {
+      while (getBet() != 0)
+      {
+      //getBet();
+      
+      // System.out.println(pull()+" ---pull");
+      // TESTING ---------------------------
+      // System.out.println(result+"----test");
+      // result.string1_change("bar");
+      // result.string2_change("space");
+      // result.string3_change("hats");
+      // result.string3_change("828282322222222222222222222222222222222222222222222222222222222282828");
+      // System.out.println(result.toString());
+      // System.out.println(randString());
+      // ThreeString result = new ThreeString();
+      // TESTING ---------------------------
+      System.out.println("whirrrrrr .... and your pull is ...");
+      getPayMultiplier (pull());
+      }
+      
+   }
 
    public static int getBet()
-   {
-      System.out.println ("GetBet");
-      return 0;
-   }
-   
-   public ThreeString pull()
-   {
-      // "This method instantiates and returns a ThreeString object to the client."
-      ThreeString rand_pull = new ThreeString();
-      return rand_pull;
+      {
+         System.out.print("How much would you like to bet (1 - 100) or 0 to quit? "); // Initial prompt for input
+         Scanner keyboard_input = new Scanner(System.in);
+         int user_bet = keyboard_input.nextInt();
+         boolean checkBet = false;
+         while (checkBet == false)
+         {
+            if (user_bet == 0)
+            {
+               System.out.println("UserQuit");
+               checkBet = true;
+            }
+            else if (user_bet >= 1 && user_bet <= 100)
+            {
+               checkBet = true;
+            }
+            else
+            {
+               System.out.print("How much would you like to bet (1 - 100) or 0 to quit? ");
+               user_bet = keyboard_input.nextInt();
+            }
+         }
+         return user_bet;
+      }
 
-   }
-
-   public static randString()
+   static ThreeString pull()
    {
-      // It produces and returns a single random string based on the required probabilities.
-      return "randomString";
+      // placeholder
+      ThreeString gamble = new ThreeString();
+      gamble.string1_change(randString());
+      gamble.string2_change(randString());
+      gamble.string3_change(randString());
+      return gamble;
    }
 
-   public static void main ( String[] args ) 
+   private static String randString()
    {
-      System.out.println("assignment 2");
+      int ranNum = (int)(Math.random()*1000);
+      String slot = "";
+      // space 1/2  (50%)
+      // System.out.println(ranNum+" rannum");
+      if (ranNum > 500)
+      {
+         slot = "space";
+      }
+      // cherries 1/4  (25%)
+      else if (ranNum > 250)
+      {
+         slot = "cherries";
+      }
+      else if (ranNum > 125)
+      {
+         // BAR 1/8  (12.5%)
+         slot = "BAR";
+      }
+      else
+      {
+         slot = "7";
+      }
+      return slot;
    }
 
-   // This get the multiplier for the winnings 
-   int getPayMultiplier (ThreeString thePull)
+   static int getPayMultiplier (ThreeString thePull)
    {
-      // After main() gets a ThreeString object from pull(), which we will call pullString, it needs to know what the payout will be.
-      return 0;
+      // System.out.println(thePull.toString());
+      // slot_pull = thePull.toString();
+      // thePull.string1_change("7");
+      // thePull.string2_change("7");
+      // thePull.string3_change("7");
+      String [] pull_slots = thePull.toString().split("  ");
+      // System.out.println( Arrays.toString(pull_slots) + "---pulltestlength");
+      // System.out.println( pull_slots[0] + "---1test");
+      // System.out.println( pull_slots[1] + "---2test");
+      // System.out.println( pull_slots[2] + "---3test");
+      //System.out.println(("cherries".equals(pull_slots[1])) + "-----equaltest");
+      System.out.println(thePull.toString());
+      
+      //  cherries  [not cherries]  [any] pays 5 × bet (5 times the bet)
+      if (pull_slots[0].equals("cherries") && !"cherries".equals(pull_slots[1]))
+      {
+         System.out.println("payout was 5x");
+      }
+      //  cherries  cherries  [not cherries] pays 15 × bet 
+      else if (pull_slots[0].equals("cherries") && pull_slots[1].equals("cherries") && !pull_slots[2].equals("cherries"))
+      {
+         System.out.println("payout was 15x");
+      }
+      //  cherries  cherries  cherries pays 30 × bet 
+      else if (pull_slots[0].equals("cherries") && pull_slots[1].equals("cherries") && pull_slots[2].equals("cherries"))
+      {
+         System.out.println("payout was 30x");
+      }
+      //  BAR  BAR  BARpays 50 × bet 
+      else if (pull_slots[0].equals("BAR") && pull_slots[1].equals("BAR") && pull_slots[2].equals("BAR"))
+      {
+         System.out.println("payout was 50x");
+      }
+      //  7  7  7 pays 100 × bet 
+      else if (pull_slots[0].equals("7") && pull_slots[1].equals("7") && pull_slots[2].equals("7"))
+      {
+         System.out.println("payout was 100x");
+      }
+   return 0;
    }
-
-   void display (ThreeString thePull, int winnings ) 
-   {
-      // displays the output
-   }
-
 }
 
 class ThreeString
@@ -52,29 +144,63 @@ class ThreeString
    private String string1;
    private String string2;
    private String string3;
-   private static int [] pullWinnings = new int[MAX_PULLS]; // In the array fill with the number that was won from the pull
-   private static int numPulls; // tracks the number of pulls that have been done
+   private static int [] pullWinnings = new int[MAX_LEN]; 
+   private static int numPulls;
 
-   // Default constructor??
-   public ThreeString()
+   ThreeString()
    {
-      ThreeString rand_pull = new ThreeString();
-      rand_pull.string1 = "";
-      rand_pull.string2 = "";
-      rand_pull.string3 = "";
+      string1 = "";
+      string2 = "";
+      string3 = "";
       
    }
 
    // Is this a mutator? Accessor? issues here 
-   boolean validString( String str )
+   private boolean validString( String str )
    {
       return (( str != null && str.length() <= MAX_LEN ));
+   }
+
+   // setters
+   // Q: Is there a way to make an argument a default but give opinion to change
+   // Can you use getters? should they be seperated?
+   // setter/getter example:
+   // private void change_string1(String str){
+   //    string1 = str;
+   // }
+   // private String return_string1(){
+   //    return string1;
+   // }
+   String string1_change(String str)
+   {
+      if (validString( str ))
+      {
+         string1 = str;
+      }
+      return string1;
+   }
+   String string2_change(String str)
+   {
+      if (validString( str ))
+      {
+         string2 = str;
+      }
+      return string2;
+   }
+   String string3_change(String str)
+   {
+      if (validString( str ))
+      {
+         string3 = str;
+      }
+      return string3;
    }
 
    // Returns 
    public String toString() 
    {
-      return string1 + "," + string2 + "," + string3;
+      String combinedString = string1 + "  " + string2 + "  " + string3;
+      return combinedString;
    }
 
    // boolean saveWinnings(int winnings), and String toStringWinnings()
@@ -99,4 +225,7 @@ class ThreeString
       return pullWinningsString;
    }
 }
+
+
+
 
